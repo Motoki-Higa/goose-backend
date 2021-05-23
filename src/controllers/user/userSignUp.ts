9196f1ec8c29f1b574236cc701f3f54b53bb269c
Motoki Higa <mokkyhiga@gmail.com>
2021-05-23 10:30:55 +0900

    fix: add env condition to signup email comfirmation link

--- src/controllers/user/userSignUp.ts
+++ src/controllers/user/userSignUp.ts
@@ -55,7 +55,7 @@ const userSignUp = async (req: Request, res: Response, next: NextFunction) => {
             <p>Please verify your email address.<br />
             This verification expires in 10 minitue. <br />
             You can re-send verification email if it's expired.</p>
-            <a href="http://localhost:3000/email/verify/${ token }">Verify my email address</a>
+            <a href="${ process.env.SITE_URL }/email/verify/${ token }">Verify my email address</a>
         `
         
         sendEmail(to, from, subject, text);
